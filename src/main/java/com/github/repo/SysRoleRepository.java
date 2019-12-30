77877bc1f5efe2b8f0eca4daeac9ea790c4fd8fc
tanshiyi <tanshiyi_work@163.com>
2019-12-30 23:36:21 +0800

    用户管理

--- src/main/java/com/github/repo/SysRoleRepository.java
+++ src/main/java/com/github/repo/SysRoleRepository.java
@@ -4,6 +4,10 @@ import com.github.entity.SysRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface SysRoleRepository extends JpaRepository<SysRole,Integer>, JpaSpecificationExecutor<SysRole> {
     SysRole findSysRoleByRoleIdIsAndAndDelFlagIs(Integer roleId,String delFlag);
+
+    List<SysRole> findAllByDelFlagIs(String delFlag);
 }
